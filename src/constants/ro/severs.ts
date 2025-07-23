ea25cc83324d0f8eddef376a08a62a94891f7d38
SDxBacon <z141134553@gmail.com>
2025-07-23 19:14:06 +0800

    feat: 🎸 add taiwan zero server list

---
+++ src/constants/ro/severs.ts
@@ -0,0 +1,11 @@
+// 女武神-西格倫
+export const Sigrun = "Sigrun";
+
+// 艾克瑟
+export const Axl = "Axl";
+
+// 台灣RO Zero伺服器常數
+export const TWROZServers = {
+  [Sigrun]: Sigrun,
+  [Axl]: Axl,
+} as const;
